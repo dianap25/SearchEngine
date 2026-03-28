fb581c17198a0b9dfc0e8eafa62bbf85d365e979
afilinko <01194269@pw.edu.pl>
2026-03-28 14:32:13 +0100

    basic methods of scanner

--- SearchEngine/include/Scanner.h
+++ SearchEngine/include/Scanner.h
@@ -1,6 +1,10 @@
 #pragma once
+#include <vector>
+#include <string>
 
 class Scanner {
 public:
-    void scan();
+    std::vector<std::string> scan(const std::string& path);
+private:
+    bool isSupported(const std::string& path);
 };
--- SearchEngine/src/Scanner.cpp
+++ SearchEngine/src/Scanner.cpp
@@ -1,7 +1,26 @@
 #include "Scanner.h"
+#include <filesystem>
 
-#include <iostream>
+std::vector<std::string> Scanner::scan(const std::string& path) {
+    std::vector<std::string> files;
+    for (const auto& entry : std::filesystem::recursive_directory_iterator(path)) {
+        if (entry.is_regular_file()) {
+            std::string filePath = entry.path().string();
+            if (isSupported(filePath)) {
+                files.push_back(filePath);
+            }
+        }
+    }
+    return files;
+}
 
-void Scanner::scan() {
-    std::cout << "Scanning files...\n";
+bool Scanner::isSupported(const std::string& path) {
+    if (path.size() >= 4 && path.substr(path.size() - 4) == ".txt")
+        return true;
+    if (path.size() >= 4 && path.substr(path.size() - 4) == ".tex")
+        return true;
+    if (path.size() >= 4 && path.substr(path.size() - 4) == ".pdf")
+        return true;
+        // files without extension
+    return path.find('.') == std::string::npos;
 }
